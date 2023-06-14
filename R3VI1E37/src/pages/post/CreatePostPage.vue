091f9226805c00234466dc1ef96c14c4ff9e97c1
Revi1337 <david122123@gmail.com>
2023-06-14 22:37:54 +0900

    Velog 처럼 직접만든 MarkDown Editor 에 Image 복사붙여넣기 구현
    * Velog 에서 이미지를 Paste 하여 에디터에 노출시키는 것처럼. textarea 로 만든 Markdown Editor 에서 ctrl + v 를 누를 시, Clipboard 에서 이미지 데이터를 갖고와 Backend 에 저장시킴과 동시에 해당 이미를 fetch 하여 갖고와 markdown 에 띄울수있도록 구현하였음.

--- R3VI1E37/src/pages/post/CreatePostPage.vue
+++ R3VI1E37/src/pages/post/CreatePostPage.vue
@@ -1,64 +1,85 @@
 <template>
   <div class="q-pt-xl row no-wrap q-col-gutter-xl" :style="{ height: '800px' }">
-    <div class="col-6">
-      <div class="q-gutter-y-lg">
-        <div>
-          <p class="text-h5">TITLE</p>
-          <q-separator dark spaced />
-          <div class="q-mt-md">
-            <q-input
-              dark
-              :spellcheck="false"
-              filled
-              autofocus
-              counter
-              v-model="createPostData.title"
-              color="light-green-12"
-              label="Title"
-              label-color="light-green-12"
-              hint="Title Must Be Specified"
-            />
-          </div>
-        </div>
+    <div class="col-6 column justify-between">
+      <div class="row no-wrap">
+        <q-select
+          spellcheck="false"
+          class="col-3 q-mr-lg"
+          color="light-green-12"
+          dark
+          label="Category"
+          label-color="light-green-12"
+          v-model="model"
+          use-input
+          hide-selected
+          fill-input
+          input-debounce="0"
+          :options="options"
+          @filter="filterFn"
+          hint="Choice One"
+          style="padding-bottom: 32px"
+        >
+          <template v-slot:no-option>
+            <q-item>
+              <q-item-section class="text-grey"> No results </q-item-section>
+            </q-item>
+          </template>
+        </q-select>
 
-        <div>
-          <p class="text-h5">CONTENT</p>
-          <q-separator dark spaced />
-          <div class="q-mt-md">
-            <q-input
-              :spellcheck="false"
-              dark
-              filled
-              counter
-              v-model="createPostData.content"
-              label="Content"
-              label-color="light-green-12"
-              type="textarea"
-              color="light-green-12"
-              :input-style="{ minHeight: '400px', lineHeight: '30px' }"
-              @keydown.tab.prevent="handlingTabKey"
-            />
-          </div>
+        <q-select
+          spellcheck="false"
+          class="col-grow"
+          dark
+          label="HashTags"
+          label-color="light-green-12"
+          v-model="model2"
+          use-input
+          use-chips
+          multiple
+          hide-dropdown-icon
+          hint="Fill Input and Press ENTER (Can Multiple)"
+          input-debounce="0"
+          @new-value="createValue"
+          style="padding-bottom: 32px"
+        />
+      </div>
+
+      <div>
+        <div class="q-mt-lg">
+          <q-input
+            dark
+            :spellcheck="false"
+            filled
+            autofocus
+            counter
+            v-model="createPostData.title"
+            color="light-green-12"
+            label="Title"
+            label-color="light-green-12"
+            hint="Title Must Be Specified"
+          />
         </div>
       </div>
 
-      <!-- <div class="text-right q-mt-xl">
-        <q-btn
-          :ripple="false"
-          label="upload"
-          outline
-          color="light-green-12"
-          class="q-mr-md"
-          @click="createPostDialog = !createPostDialog"
-        />
-        <q-btn
-          :ripple="false"
-          label="back"
-          outline
-          color="light-green-12"
-          @click="$router.push({ name: 'Index' })"
-        />
-      </div> -->
+      <div>
+        <!-- @keydown.ctrl.v.prevent="handlingPaste" -->
+        <div class="q-mt-md">
+          <q-input
+            :spellcheck="false"
+            dark
+            filled
+            counter
+            v-model="createPostData.content"
+            label="Content"
+            label-color="light-green-12"
+            type="textarea"
+            color="light-green-12"
+            :input-style="{ minHeight: '400px', lineHeight: '30px' }"
+            @keydown.tab.prevent="handlingTabKey"
+            @paste="event => handlingPaste(event)"
+          />
+        </div>
+      </div>
     </div>
 
     <div class="col-6 mark-down scroll">
@@ -66,7 +87,10 @@
       <q-separator dark spaced />
       <div class="markdown-container" v-html="markdownToHtml"></div>
     </div>
+
+    <!-- <img v-if="imageDataURL" :src="imageDataURL" alt="Pasted Image" /> -->
   </div>
+
   <div class="text-right q-pt-md q-mr-xl">
     <q-btn
       :ripple="false"
@@ -107,6 +131,21 @@ import 'highlight.js/styles/atom-one-dark.css';
 import { createPost } from 'src/api/posts';
 import { useRouter } from 'vue-router';
 
+// Select Category & Hashtag
+const stringOptions = ['DEV', 'CTF', 'WRITEUP', 'CS', 'CHEETSHEET'];
+const options = ref(stringOptions);
+const model = ref(null);
+const filterFn = (val, update, abort) => {
+  update(() => {
+    const needle = val.toLowerCase();
+    options.value = stringOptions.filter(v => v.toLowerCase().indexOf(needle) > -1);
+  });
+};
+const model2 = ref(null);
+const createValue = (val, done) => {
+  done(val, 'add-unique');
+};
+
 // routing
 const router = useRouter();
 
@@ -114,58 +153,7 @@ const router = useRouter();
 const createPostData = ref({
   title: 'Title',
   content: `
-| Tables        | Are           | Cool  |
-| ------------- |:-------------:| -----:|
-| col 3 is      | right-aligned | $1600 |
-| col 2 is      | centered      |   $12 |
-| zebra stripes | are neat      |    $1 |
-# PC
-asdasdasdasdDUMMY\n
-asdasdasdasdDUMMY\n
-asdasdasdasdDUMMY\n
-### Port Scan
-\`\`\`bash
-$nmap
-\`\`\`
-asdasdasdasdDUMMY\n
-asdasdasdasdDUMMY\n
-asdasdasdasdDUMMY\n
-asdasdasdasdDUMMY\n
-### Information Gathering
-asdasd \`as\` dasdDUMMY\n
-asdasdasdasdDUMMY\n
-asdasdasdasdDUMMY\n
-### SQL Injection (SQLite3)
-asd \`DUMMY\` \n
-asdasdasdasdDUMMY\n
-asdasdasdasdDUMMY\n
-### Initial Access
-asdasdasdasdDUMMY\n
-asdasdasdasdDUMMY\n
-asdasdasdasdDUMMY\n
-### Network Pivoting (SSH Tunneling)
-asdasdasdasdDUMMY\n
-asdasdasdasdDUMMY\n
-### 1-Day Exploit(CVE-2023-0297)
-asdasdasdasdDUMMY\n
-asdasdasdasdDUMMY\n
-asdasdasdasdDUMMY\n
-asdasdasdasdDUMMY\n
-### Privilege Escalation
-asdasdasdasdDUMMY\n
-asdasdasdasdDUMMY\n
-asdasdasdasdDUMMY\n
-\`\`\`bash
-$curl 10.10.11.211 -I
-HTTP/1.1 200 OK
-Server: nginx/1.18.0 (Ubuntu)
-...
-X-Powered-By: PHP/7.4.33
-...
-P3P: CP="CAO PSA OUR"
-...
-Set-Cookie: Cacti=3a7ee08d96abc903030aba5226681aad; path=/; HttpOnly; SameSite=Strict
-\`\`\`
+  * Dummy
   `,
   hashtag: 'dummyTag'
 });
@@ -209,4 +197,48 @@ const handlingTabKey = event => {
   refer.value = newValue;
   refer.setSelectionRange(currentPosition + 4, currentPosition + 4);
 };
+
+// Handling Copy & Paste
+const imageDataURL = ref(null);
+
+const handlingPaste = event => {
+  const clipboardData = event.clipboardData || window.clipboardData;
+  const items = clipboardData.items;
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].type.indexOf('image') !== -1) {
+      const imageFile = items[i].getAsFile(); // File Object
+      uploadImage(imageFile);
+    }
+  }
+};
+
+import axios from 'axios';
+const uploadImage = async imageFile => {
+  const formData = new FormData();
+  formData.append('file', imageFile, imageFile.name);
+  const { data } = await axios.post('/api/post/image', formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
+  });
+  createPostData.value.content += `![](http://localhost:8080/api/post/image/${data.replace('.png', '')})`;
+};
 </script>
+
+<!-- const handlingPaste = event => {
+  const clipboardData = event.clipboardData || window.clipboardData;
+  const items = clipboardData.items;
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].type.indexOf('image') !== -1) {
+      const imageFile = items[i].getAsFile(); // File Object
+      const base64DataURL = toDataURL(imageFile);
+      console.log(base64DataURL)
+    }
+  }
+};
+
+const toDataURL = imageFile => {
+  const reader = new FileReader();
+  reader.onload = event => (imageDataURL.value = event.target.result);
+  reader.readAsDataURL(imageFile); // looks like data:image/png;base64,iVBORw0K...
+}; -->
