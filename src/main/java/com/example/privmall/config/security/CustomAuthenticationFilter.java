c025b9bd4b492691ba75f89e700b2b614c15fda0
Revi1337 <david122123@gmail.com>
2023-06-18 16:14:24 +0900

    로그인 성공 시, 토큰과 유저정보를 함께 응답으로 보냄

--- src/main/java/com/example/privmall/config/security/CustomAuthenticationFilter.java
+++ src/main/java/com/example/privmall/config/security/CustomAuthenticationFilter.java
@@ -1,13 +1,17 @@
 package com.example.privmall.config.security;
 
+import com.example.privmall.domain.UserAccount;
+import com.example.privmall.dto.UserAccountDto;
 import com.example.privmall.dto.request.principal.UserAccountPrincipal;
 import com.example.privmall.dto.request.LoginRequest;
+import com.example.privmall.dto.response.UserAccountResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -15,6 +19,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -50,7 +55,14 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
                                             Authentication authResult) throws IOException, ServletException {
         System.out.println("CustomAuthenticationFilter.successfulAuthentication");
         UserAccountPrincipal principal = (UserAccountPrincipal) authResult.getPrincipal();
+        System.out.println(principal.nickname());
+        String userResponse = objectMapper.writeValueAsString(
+                UserAccountResponse.from(principal.toDto())
+        );
         response.addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + jwtUtil.generate(principal.getUsername()));
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        PrintWriter writer = response.getWriter();
+        writer.println(userResponse);
     }
 
     @Override
