ff783a6910b399e9556835d6eaf89f5089085739
Revi1337 <david122123@gmail.com>
2023-06-20 22:59:43 +0900

    [GitHubAPI 연동, TryHackeMe API 연동]

--- R3VI1E37/src/pages/IndexPage.vue
+++ R3VI1E37/src/pages/IndexPage.vue
@@ -1,8 +1,8 @@
 <template>
   <p class="text-h5 text-weight-bold">Recent Post</p>
   <q-carousel
-    :style="{ height: 'auto', width: 'auto' }"
     class="carousel"
+    :style="{ height: 'auto', width: 'auto' }"
     dark
     swipeable
     animated
@@ -36,13 +36,13 @@
               </div>
 
               <div class="col-7 row items-center justify-center q-ml-xl">
-                <div class="q-ma-lg q-gutter-y-md">
+                <div class="q-gutter-y-md">
                   <div>
-                    <q-avatar size="md">
+                    <!-- <q-avatar size="2rem">
                       <q-img
-                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZHnYW3u0Y2pKCoepqCtCchGi89SaRO4_oZWyg7ial3BmDSAqGElB_LMIqmWEIiTUCpLs&usqp=CAU"
+                        src="https://encrypted-tbn0.gstatic.com/images?q=t bn:ANd9GcQZHnYW3u0Y2pKCoepqCtCchGi89SaRO4_oZWyg7ial3BmDSAqGElB_LMIqmWEIiTUCpLs&usqp=CAU"
                       />
-                    </q-avatar>
+                    </q-avatar> -->
                     <span class="q-ml-sm">{{ post.createdBy }}</span>
                   </div>
 
@@ -65,13 +65,78 @@
       </q-carousel-slide>
     </template>
   </q-carousel>
+
+  <p class="q-mt-lg text-h5 text-weight-bold">Development Log</p>
+  <q-table
+    style="width: 650px"
+    dense
+    hide-no-data
+    dark
+    flat
+    bordered
+    :rows="githubResults"
+    :columns="columns"
+    row-key="name"
+    separator="cell"
+  >
+    <template v-slot:top>
+      <q-avatar font-size="1.8rem" size="1.8rem" icon="fa-brands fa-github" />
+    </template>
+  </q-table>
+
+  <div>
+    <p class="q-mt-lg text-h5 text-weight-bold">TryHackMe Log</p>
+    <div class="row items-end justify-center no-wrap q-gutter-x-md" :style="{ width: '250px' }">
+      <div>
+        <span>&nbsp;</span>
+        <div class="text-center text-h6 text-weight-bold">{{ tryHackMeTotalUsers.totalUsers }}</div>
+        <div class="text-center text-caption">
+          <q-icon name="fa-solid fa-users" />
+          Users
+        </div>
+      </div>
+      <div>
+        <span v-if="!isNaN(tryHackMeRankPercentage)">
+          <span class="text-caption text-light-green-12"> In the top {{ tryHackMeRankPercentage }}%</span>
+        </span>
+        <div class="text-center text-h6 text-weight-bold">{{ tryHackMeUserRank.userRank }}</div>
+        <div class="text-center text-caption">
+          <q-icon name="fa-solid fa-trophy" />
+          Rank
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div>
+    <ChartComponent v-if="loaded" chart-type="radar" :chart-data="chartData" :chart-options="config" />
+  </div>
+
+  <!-- <Radar v-if="loaded" :data="chartData" :options="config" /> -->
 </template>
 
 <script setup>
-import { ref } from 'vue';
 import { findAllPost } from 'src/api/posts';
 import PostComponent from 'src/components/PostComponent.vue';
-import { onMounted } from 'vue';
+import ChartComponent from 'src/components/ChartComponent.vue';
+import { onMounted, computed, ref } from 'vue';
+import {
+  fetchGithubCommits,
+  fetchTryHackMeTotalUsers,
+  fetchTryHackMeRank,
+  fetchTryHackMeSkillSet
+} from 'src/api/data';
+
+import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend
+} from 'chart.js';
+import { Radar } from 'vue-chartjs';
 
 // request Recent post
 const posts = ref([]);
@@ -93,8 +158,139 @@ const findRecentPostRequest = async param => {
 const slide = ref(1);
 const autoplay = ref(3000);
 
-onMounted(() => {
+// fetch github api
+const githubCommits = ref([]);
+const githubResults = ref([]);
+const fetchGithubCommitsRequest = async repository => {
+  ({ data: githubCommits.value } = await fetchGithubCommits(repository));
+  githubCommits.value.forEach(({ commit }) =>
+    githubResults.value.push({
+      message: commit.message.slice(0, 30),
+      committer: commit.committer.name,
+      email: commit.committer.email,
+      date: commit.committer.date
+    })
+  );
+};
+
+// fetch TryHackMe TotalUser
+const tryHackMeTotalUsers = ref({});
+const fetchTryHackMeTotalUsersRequest = async () => {
+  ({ data: tryHackMeTotalUsers.value } = await fetchTryHackMeTotalUsers());
+};
+
+// fetch TryHackMe UserRank
+const tryHackMeUserRank = ref({});
+const fetchTryHackMeRankRequest = async () => {
+  ({ data: tryHackMeUserRank.value } = await fetchTryHackMeRank());
+};
+
+// calculate TryHackMe RankPercentage
+const tryHackMeRankPercentage = computed(() => {
+  return Math.ceil((tryHackMeUserRank.value.userRank / tryHackMeTotalUsers.value.totalUsers) * 100);
+});
+
+// fetch TryHackMe SkillSet & ChartJS
+const tryHackMeSkillSet = ref({
+  fundamentals: '',
+  linux: '',
+  network: '',
+  privesc: '',
+  web: '',
+  windows: ''
+});
+const fetchTryHackMeSkillSetRequest = async () => {
+  loaded.value = false;
+  const { data } = await fetchTryHackMeSkillSet();
+  chartData.value = {
+    labels: Object.keys(tryHackMeSkillSet.value),
+    datasets: [
+      {
+        label: 'Experiment Score',
+        data: [
+          data.skills.fundamentals.score,
+          data.skills.linux.score,
+          data.skills.network.score,
+          data.skills.privesc.score,
+          data.skills.web.score,
+          data.skills.windows.score
+        ],
+        fill: true,
+        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        borderColor: 'rgb(54, 162, 235)',
+        pointBackgroundColor: 'rgb(54, 162, 235)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgb(54, 162, 235)'
+      }
+    ]
+  };
+  loaded.value = true;
+};
+ChartJS.register(RadialLinearScale, PointElement, LineElement, Filler, Tooltip, Legend);
+ChartJS.defaults.color = '#BADA55';
+ChartJS.defaults.borderColor = '#adbac7';
+const loaded = ref(false);
+const chartData = ref(null);
+const config = {
+  elements: {
+    line: {
+      borderWidth: 3
+    }
+  },
+  scales: {
+    r: {
+      beginAtZero: true,
+      min: 0,
+      max: 150,
+      backgroundColor: 'rgba(0, 0, 0, 0.8)',
+      ticks: {
+        stepSize: 10,
+        callback: (value, tick, values) => '',
+        backdropPadding: 0
+      }
+      // angleLines: {
+      //   color: 'red'
+      // }
+    }
+  },
+  animation: {
+    duration: 1500,
+    easing: 'easeInOutQuart'
+  }
+};
+
+// table
+const columns = [
+  {
+    name: 'message',
+    label: 'Message',
+    field: 'message',
+    align: 'left',
+    sortable: true
+  },
+  {
+    name: 'committer',
+    label: 'Committer',
+    field: 'committer',
+    align: 'center'
+  },
+  {
+    name: 'date',
+    label: 'Date',
+    field: 'date',
+    align: 'center',
+    sortable: true
+  }
+];
+
+// LifeCycle Hook
+onMounted(async () => {
   findRecentPostRequest(postRequestParam.value); // TODO: 프롭스로 넘기든 뭐든 해야한다.
+  fetchGithubCommitsRequest('R3VI1E37');
+  fetchTryHackMeTotalUsersRequest();
+  fetchTryHackMeRankRequest('revi1337');
+  fetchTryHackMeSkillSetRequest();
 });
 </script>
 
