ff5d3a75947387ecc80465530cd86100f3353a81
Revi1337 <david122123@gmail.com>
2023-06-24 22:13:06 +0900

    [Back : Test 코드 작성 시 인증객체 주입]
    * 테스트코드를 진행할떄 인증된 사용자가 필요한 테스트를 수행하는 경우, 인증객체가 필요.
    * 따라서 `WithSecurityContextFactory` 를 impl 하여 테스트메서드에 달아주면, 인증객체가 주입되게 만들어주었음.

---
+++ src/test/java/com/example/privmall/WithAuthUser.java
@@ -0,0 +1,13 @@
+package com.example.privmall;
+
+import org.springframework.security.test.context.support.WithSecurityContext;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithSecurityContext(factory = WithAuthUserSecurityContextFactory.class)
+public @interface WithAuthUser {
+    String email() default "dummyUser@gmail.com";
+    String[] roles() default {"ROLE_USER"};
+}
